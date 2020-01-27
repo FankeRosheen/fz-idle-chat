f7f33a3d7357314000a1100497cca83c75d3181d
czbcxy <k995211314@163.com>
2020-01-27 19:40:40 +0800

    first commit

---
+++ chat-producer/src/test/java/fzidlechat/chatproducer/ChatProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package fzidlechat.chatproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
